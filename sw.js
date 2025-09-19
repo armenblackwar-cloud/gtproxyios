0f57a7e814a5295dc03bd73cbab16e226bc90d59
armenblackwar-cloud <armenblackwar@gmail.com>
2025-09-19 18:11:35 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,6 @@
-const CACHE_NAME = "gt-dashboard-v1";
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-// Установка SW и кеширование
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open("gt-cache").then(c=>c.addAll(["./","./index.html","./manifest.json"])));
 });
-
-// Активация и очистка старого кэша
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
-    )
-  );
-  self.clients.claim();
-});
-
-// Перехват запросов
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
